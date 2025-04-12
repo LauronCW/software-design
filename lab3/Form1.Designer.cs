6c13ea49de5db49dbfaa72b6bae1f84dce315e13
LauronCW <c.lauron.560394@umindanao.edu.ph>
2025-04-13 00:40:15 +0800

    Exercise 3

--- lab3/Form1.Designer.cs
+++ lab3/Form1.Designer.cs
@@ -20,20 +20,140 @@
 			base.Dispose(disposing);
 		}
 
-		#region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-		/// <summary>
-		///  Required method for Designer support - do not modify
-		///  the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
-		}
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnShowInfo = new Button();
+            lblTitle = new Label();
+            lblAuthor = new Label();
+            lblExtra = new Label();
+            listBooks = new ListBox();
+            btnShowAll = new Button();
+            Title = new Label();
+            Author = new Label();
+            Issue = new Label();
+            SuspendLayout();
+            // 
+            // btnShowInfo
+            // 
+            btnShowInfo.Location = new Point(138, 192);
+            btnShowInfo.Name = "btnShowInfo";
+            btnShowInfo.Size = new Size(94, 29);
+            btnShowInfo.TabIndex = 0;
+            btnShowInfo.Text = "Show Info";
+            btnShowInfo.UseVisualStyleBackColor = true;
+            btnShowInfo.Click += btnShowInfo_Click;
+            // 
+            // lblTitle
+            // 
+            lblTitle.AutoSize = true;
+            lblTitle.Location = new Point(138, 85);
+            lblTitle.Name = "lblTitle";
+            lblTitle.Size = new Size(41, 20);
+            lblTitle.TabIndex = 1;
+            lblTitle.Text = "Title:";
+            lblTitle.Click += btnShowInfo_Click;
+            // 
+            // lblAuthor
+            // 
+            lblAuthor.AutoSize = true;
+            lblAuthor.Location = new Point(138, 118);
+            lblAuthor.Name = "lblAuthor";
+            lblAuthor.Size = new Size(57, 20);
+            lblAuthor.TabIndex = 2;
+            lblAuthor.Text = "Author:";
+            lblAuthor.Click += btnShowInfo_Click;
+            // 
+            // lblExtra
+            // 
+            lblExtra.AutoSize = true;
+            lblExtra.Location = new Point(138, 151);
+            lblExtra.Name = "lblExtra";
+            lblExtra.Size = new Size(61, 20);
+            lblExtra.TabIndex = 3;
+            lblExtra.Text = "Issue # :";
+            lblExtra.Click += btnShowInfo_Click;
+            // 
+            // listBooks
+            // 
+            listBooks.FormattingEnabled = true;
+            listBooks.Location = new Point(138, 247);
+            listBooks.Name = "listBooks";
+            listBooks.Size = new Size(573, 324);
+            listBooks.TabIndex = 4;
+            // 
+            // btnShowAll
+            // 
+            btnShowAll.Location = new Point(138, 587);
+            btnShowAll.Name = "btnShowAll";
+            btnShowAll.Size = new Size(94, 29);
+            btnShowAll.TabIndex = 5;
+            btnShowAll.Text = "Show All";
+            btnShowAll.UseVisualStyleBackColor = true;
+            btnShowAll.Click += btnShowAll_Click;
+            // 
+            // Title
+            // 
+            Title.AutoSize = true;
+            Title.Location = new Point(185, 85);
+            Title.Name = "Title";
+            Title.Size = new Size(76, 20);
+            Title.TabIndex = 6;
+            Title.Text = "Book Title";
+            // 
+            // Author
+            // 
+            Author.AutoSize = true;
+            Author.Location = new Point(201, 118);
+            Author.Name = "Author";
+            Author.Size = new Size(49, 20);
+            Author.TabIndex = 7;
+            Author.Text = "Name";
+            // 
+            // Issue
+            // 
+            Issue.AutoSize = true;
+            Issue.Location = new Point(205, 151);
+            Issue.Name = "Issue";
+            Issue.Size = new Size(99, 20);
+            Issue.TabIndex = 8;
+            Issue.Text = "Issue Number";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(786, 660);
+            Controls.Add(Issue);
+            Controls.Add(Author);
+            Controls.Add(Title);
+            Controls.Add(btnShowAll);
+            Controls.Add(listBooks);
+            Controls.Add(lblExtra);
+            Controls.Add(lblAuthor);
+            Controls.Add(lblTitle);
+            Controls.Add(btnShowInfo);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
 
-		#endregion
-	}
+        #endregion
+
+        private Button btnShowInfo;
+        private Label lblTitle;
+        private Label lblAuthor;
+        private Label lblExtra;
+        private ListBox listBooks;
+        private Button btnShowAll;
+        private Label Title;
+        private Label Author;
+        private Label Issue;
+    }
 }
--- lab3/Form1.cs
+++ lab3/Form1.cs
@@ -6,5 +6,99 @@ namespace lab3
 		{
 			InitializeComponent();
 		}
-	}
+
+		public class Book
+		{
+			public string Title { get; set; }
+			public string Author { get; set; }
+			public virtual string GetInfo()
+			{
+				return $"{Title} by {Author}";
+			}
+		}
+
+
+		//SUBCLASSESS
+		public class Magazine : Book
+		{
+			public int IssueNumber { get; set; }
+			public override string GetInfo()
+			{
+				return $"{Title} - Issue {IssueNumber} by {Author}";
+			}
+
+		}
+
+
+		
+
+		public class Ebook : Book
+		{
+			public double FileSizeMB { get; set; }
+            public override string GetInfo()
+            {
+				return $"{Title} (Ebook) - {FileSizeMB}MB by {Author}";
+            }
+        }
+
+
+		public class Textbook : Book
+		{
+			public string Subject { get; set; }
+            public override string GetInfo()
+            {
+				return $"{Title} (Textbook) - Subject: {Subject} by {Author}";
+            }
+        }
+
+
+
+		public class Audiobook : Book
+		{
+			public string Narrator { get; set; }
+			public double DurationHours { get; set; }
+            public override string GetInfo()
+            {
+				return $"{Title} (Audiobook) - Narrated by {Narrator},and written by {Author}, {DurationHours} hrs";
+            }
+        }
+
+        private void btnShowInfo_Click(object sender, EventArgs e)
+        {
+            Book myBook = new Magazine
+            {
+                Title = "Dune Messiah",
+                Author = "Frank Herbert",
+                IssueNumber = 23
+            };
+			//MessageBox.Show(myBook.GetInfo());
+			Title.Text = " " + myBook.Title;
+            Author.Text = myBook.Author;
+            //Issue.Text = myBook.IssueNumber;
+            Issue.Text = ((Magazine)myBook).IssueNumber.ToString();
+
+        }
+
+
+        private void btnShowAll_Click(object sender, EventArgs e) {
+			Book[] books = new Book[]
+			{
+				new Book { Title = "- To Kill A Mockingbird", Author = "Harper Lee"},
+				new Magazine { Title = "- National Geographic: Cosmos", Author ="Ann Druyan", IssueNumber = 3},
+				new Ebook { Title ="- Divergent", Author ="Veronica Roth", FileSizeMB = 4.5},
+				new Textbook { Title ="- Astrophysics 101", Author ="Dr.Brian Cox", Subject = "Physics"},
+				new Audiobook { Title ="- What's Up?", Author = "Ben Stiller", Narrator ="Adam Scott", DurationHours = 4.3}
+			};
+			DisplayBooks(books);
+		}
+        public void DisplayBooks(Book[] books)
+        {
+            listBooks.Items.Clear();
+            foreach (Book book in books)
+            {
+                listBooks.Items.Add(book.GetInfo());
+            }
+
+        }
+    }
 }
